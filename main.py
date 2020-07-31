0d1b4378d51740129e96c03cf0ec2507d4f4e199
Alex4386 <psh010209@gmail.com>
2020-07-31 15:00:00 +0900

    fix: to was_seen

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def classification_session(webcam: VideoStream):
                 print("Terminate: Reverting Face ID to "+str(face_uuid))
         
         if face.should_capture():
-            if face.seen_frames <= 1:
+            if not face.was_seen:
                 if face.screenshot_count == 0:
                     print("Capturing Face: Face ID: {}".format(face.uuid), flush=True)
                 else:
